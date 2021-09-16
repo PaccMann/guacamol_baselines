b05b47f2ea2f45a3a03ba3e1183fb036786842c0
mjbuchan <matthew.buchan@chch.ox.ac.uk>
2021-09-16 10:37:11 +0100

    Bug: data download directory

--- fetch_guacamol_dataset.sh
+++ fetch_guacamol_dataset.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
-mkdir -p data
+mkdir -p guacamol_baselines/data
 
-wget https://ndownloader.figshare.com/files/13612745 -O data/guacamol_v1_all.smiles
+wget https://ndownloader.figshare.com/files/13612745 -O guacamol_baselines/data/guacamol_v1_all.smiles
 
-wget https://ndownloader.figshare.com/files/13612760 -O data/guacamol_v1_train.smiles
+wget https://ndownloader.figshare.com/files/13612760 -O guacamol_baselines/data/guacamol_v1_train.smiles
 
-wget https://ndownloader.figshare.com/files/13612766 -O data/guacamol_v1_valid.smiles
+wget https://ndownloader.figshare.com/files/13612766 -O guacamol_baselines/data/guacamol_v1_valid.smiles
 
-wget https://ndownloader.figshare.com/files/13612757 -O data/guacamol_v1_test.smiles
+wget https://ndownloader.figshare.com/files/13612757 -O guacamol_baselines/data/guacamol_v1_test.smiles
 
