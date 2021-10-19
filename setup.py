e3191e33d264db258ff1857944a1a0a9a95d249e
Ashish Dave <Ashish13898@gmail.com>
2021-10-19 13:06:22 +0100

    feat: updated structure for fetching guacamol datasets

--- setup.py
+++ setup.py
@@ -1,9 +1,68 @@
 import io
 import re
 import os
-
+import subprocess
 from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
+from setuptools import setup, find_packages, Command
+from setuptools.command.bdist_egg import bdist_egg as _bdist_egg
+from setuptools.command.develop import develop as _develop
+from distutils.command.build import build as _build
+
+SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class setup_cosifer(Command):
+    """
+    Run installation to fetch guacamol datasets.
+    """
+
+
+    def initialize_options(self):
+        """Set initialize options."""
+        pass
+
+    def finalize_options(self):
+        """Set finalize options."""
+        pass
+
+    def run(self):
+        """Run installation to fetch guacamol datasets."""
+        try:
+            subprocess.check_call(
+                [os.path.join(SETUP_DIR, "fetch_guacamol_dataset.sh")]
+            )
+        except subprocess.CalledProcessError as error:
+            raise EnvironmentError(
+                f"Failed to fetch of guacamol datasets dependencies via {error.cmd}."
+            )
+
+
+class build(_build):
+    """Build command."""
+
+    sub_commands = [("setup_cosifer", None)] + _build.sub_commands
+
+
+class bdist_egg(_bdist_egg):
+    """Build bdist_egg."""
+
+    def run(self):
+        """Run build bdist_egg."""
+        self.run_command("setup_cosifer")
+        _bdist_egg.run(self)
+
+
+class develop(_develop):
+    """Build develop."""
+
+    def run(self):
+        """Run build develop."""
+        setup_cosifer = self.distribution.get_command_obj("setup_cosifer")
+        setup_cosifer.develop = True
+        self.run_command("setup_cosifer")
+        _develop.run(self)
+
 
 # ease installation during development
 vcs = re.compile(r"(git|svn|hg|bzr)\+")
@@ -43,6 +102,12 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
+    cmdclass={
+        "bdist_egg": bdist_egg,
+        "build": build,
+        "setup_cosifer": setup_cosifer,
+        "develop": develop,
+    },
     install_requires=[
         "guacamol",
         "matplotlib",
@@ -55,6 +120,3 @@ setup(
         "flake8",
     ],
 )
-
-# some baselines require the guacamol dataset to run
-os.system("bash fetch_guacamol_dataset.sh")
