4c2ee2df72d219a7a68f3ef82167d32a23a526fc
Ashish Dave <Ashish13898@gmail.com>
2021-10-20 14:27:05 +0100

    feat: modified file location for storing SMILES

--- fetch_guacamol_dataset.sh
+++ fetch_guacamol_dataset.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
-mkdir -p guacamol_baselines/data
+mkdir -p lib/guacamol_baselines/data
 
-wget https://ndownloader.figshare.com/files/13612745 -O guacamol_baselines/data/guacamol_v1_all.smiles
+wget https://ndownloader.figshare.com/files/13612745 -O lib/guacamol_baselines/data/guacamol_v1_all.smiles
 
-wget https://ndownloader.figshare.com/files/13612760 -O guacamol_baselines/data/guacamol_v1_train.smiles
+wget https://ndownloader.figshare.com/files/13612760 -O lib/guacamol_baselines/data/guacamol_v1_train.smiles
 
-wget https://ndownloader.figshare.com/files/13612766 -O guacamol_baselines/data/guacamol_v1_valid.smiles
+wget https://ndownloader.figshare.com/files/13612766 -O lib/guacamol_baselines/data/guacamol_v1_valid.smiles
 
-wget https://ndownloader.figshare.com/files/13612757 -O guacamol_baselines/data/guacamol_v1_test.smiles
+wget https://ndownloader.figshare.com/files/13612757 -O lib/guacamol_baselines/data/guacamol_v1_test.smiles
 
