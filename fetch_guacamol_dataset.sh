a2949f6486ade962dd3cee41984a03b4f3673d27
Ashish Dave <Ashish13898@gmail.com>
2021-10-25 10:41:11 +0100

    fix: added check for wget command and using curl to download smiles if wget is not installed

--- fetch_guacamol_dataset.sh
+++ fetch_guacamol_dataset.sh
@@ -4,11 +4,17 @@ build_directory=$1
 
 cd $build_directory
 
-wget https://ndownloader.figshare.com/files/13612745 -O guacamol_v1_all.smiles
+# checking if wget is installed
+if which wget >/dev/null; then
+    wget https://ndownloader.figshare.com/files/13612745 -O guacamol_v1_all.smiles
+    wget https://ndownloader.figshare.com/files/13612760 -O guacamol_v1_train.smiles
+    wget https://ndownloader.figshare.com/files/13612766 -O guacamol_v1_valid.smiles
+    wget https://ndownloader.figshare.com/files/13612757 -O guacamol_v1_test.smiles
 
-wget https://ndownloader.figshare.com/files/13612760 -O guacamol_v1_train.smiles
-
-wget https://ndownloader.figshare.com/files/13612766 -O guacamol_v1_valid.smiles
-
-wget https://ndownloader.figshare.com/files/13612757 -O guacamol_v1_test.smiles
-
+# if wget is not found then use curl to download guacamol smiles
+else
+    curl -L  https://ndownloader.figshare.com/files/13612745 -o guacamol_v1_all.smiles
+    curl -L  https://ndownloader.figshare.com/files/13612760 -o guacamol_v1_train.smiles
+    curl -L  https://ndownloader.figshare.com/files/13612766 -o guacamol_v1_valid.smiles
+    curl -L  https://ndownloader.figshare.com/files/13612757 -o guacamol_v1_test.smiles
+fi
